769a0939f1f033aff6e03e6405cb8059226e4354
gabrielmelobatista <gabrielmelobatista@hotmail.com>
2025-09-20 10:21:24 -0300

    Adicionado a homepage

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\WelcomeController;
 
-
-Route::get('/', [PortfolioController::class, 'index']);
-
+Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/aboutme', [PortfolioController::class, 'index']);
